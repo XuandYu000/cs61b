63af4a1cea3fcb07c82e80df07ec5c45d2c40719
XuandYu000 <1185819400@qq.com>
2023-09-11 17:01:18 +0800

    finish all lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,13 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList L = A;
+        while(L.rest != null){
+            L = L.rest;
+        }
+        L.rest = B;
+
+        return A;
     }
 
     /**
@@ -91,7 +97,13 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null && B == null){
+            return null;
+        }
+        if(A != null) {
+            return new IntList(A.first, catenate(A.rest, B));
+        }
+        return new IntList(B.first, catenate(A, B.rest));
     }
 
 
