352930d9e7452ad729c12859f7dbd437d5c7de98
XuandYu000 <1185819400@qq.com>
2023-09-24 22:58:57 +0800

    Finish the proj1gold

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -2,14 +2,11 @@ public class OffByN implements CharacterComparator{
     private int offSet;
 
     public OffByN(int N){
-        offSet = N;
+        this.offSet = N;
     }
 
     @Override
     public boolean equalChars(char x, char y){
-        if(x - y == offSet || y - x == offSet){
-            return true;
-        }
-        return false;
+        return Math.abs(x - y) == n;
     }
 }
