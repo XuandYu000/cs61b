a80d83cc7325ca334a8493aff0b0f6be4891ae00
XuandYu000 <xuzhiyu2022@gmail.com>
2024-01-02 21:16:26 +0800

    Finish PercolationStats.

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,69 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import org.junit.Assert;
 
+import java.util.Map;
+import java.util.Random;
+
+public class PercolationStats {
+    private int N;
+    private int T;
+    private PercolationFactory pf;
+    private double[] thresholds;
+
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        this.thresholds = new double[T];
+
+        simulate(T);
+    }
+
+    public void simulate(int T) {
+        for(int i = 0; i < T; i++) {
+            Percolation expr = pf.make(N);
+            while(!expr.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                expr.open(row, col);
+            }
+            double threshold = (double) expr.numberOfOpenSites() / (N * N);
+            thresholds[i] = threshold;
+        }
+    }
+
+    /**
+     * Return sample mean of percolation threshold
+     */
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    /*
+    * Return sample standard deviation of percolation thresshold
+    * */
+    public double stddev() {
+       return StdStats.stddev(thresholds);
+    }
+
+
+    /*
+    * Return low endpoint of 95% confidence interval
+    * */
+    public double confidenceLow() {
+        return mean() - (1.96 * stddev() / Math.sqrt(T));
+    }
+
+    /*
+    * Return high endpoint of 95% confidence interval
+    * */
+    public double confidenceHIgh() {
+        return mean() + (1.96 * stddev() / Math.sqrt(T));
+    }
 }
